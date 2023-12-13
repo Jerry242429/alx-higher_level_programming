3e119052ab0e7c1e9aa3dd3c703afae4f82434b0
Jerry242429 <eyerusalem008@gmail.com>
2023-12-13 09:27:28 -0800

    python almost circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,3 +12,19 @@ class Square(Rectangle):
         """Display string info about this square"""
         return ("[Square] ({}) {:d}/{:d} - {:d}".
                 format(self.id, self.x, self.y, self.size))
+
+     @property
+    def size(self):
+        """Property retriever for square"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Property setter for width of square."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+
+        self.width = value
+        self.height = value
