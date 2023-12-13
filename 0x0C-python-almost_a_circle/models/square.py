6483345156c20d0cab9fdc37a57fa603f2b8ed21
Jerry242429 <eyerusalem008@gmail.com>
2023-12-13 10:43:55 -0800

    python almost circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -53,17 +53,6 @@ class Square(Rectangle):
                     setattr(self, 'height', value)
                 else:
                     setattr(self, key, value)
-    def __update(self, id-None, sixe=None, x=None, y-None):
-        """method that uppdate atrribute"""
-        if id is not None:
-            self.id = id
-        if size is not None:
-            self.size = size
-        if x is not None:
-            self.x = x
-        if y is not None:
-            self.y = y
-
     def to_dictionary(self):
         """Returns the dictionary representation of a Square.
 
