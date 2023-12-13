755124508929ab6aec8c64f2fce61162f53c87a8
Jerry242429 <eyerusalem008@gmail.com>
2023-12-13 09:47:05 -0800

    python almsot circle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,55 +1,28 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
-
+"""Module that define class of circle"""
 
 from inspect import classify_class_attrs
 from models.rectangle import Rectangle
 
-
 class Square(Rectangle):
-    """Class that defines properties of Square.
-
-     Attributes:
-        width (int): width of rectangle.
-        height (int): height of rectangle.
-        x (int): x.
-        y (int): y.
-        id (int): identity of square.
-    """
+    """Class that defines properties of Square."""
     def __init__(self, size, x=0, y=0, id=None):
-        """Creates new instances of Square
-
-        Args:
-            size (int): width and height of square.
-            x (int, optional): x. Defaults to 0.
-            y (int, optional): y. Defaults to 0.
-            id (int, optional): Identity number of square. Defaults to None.
-        """
+        """Creates new instances of Square"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """Prints square"""
+        """Return string square"""
         return ("[Square] ({}) {:d}/{:d} - {:d}".
                 format(self.id, self.x, self.y, self.size))
 
     @property
     def size(self):
-        """Property retriever for size.
-
-        Returns:
-            int: size of one side of square.
-        """
+        """size of this square"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Property setter for width of square.
-        Args:
-            value (int): width of square.
-        Raises:
-            TypeError: if width is not an integer.
-            ValueError: if width is less than or equal to zero.
-        """
+        """Property setter for width of square."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
@@ -80,6 +53,16 @@ class Square(Rectangle):
                     setattr(self, 'height', value)
                 else:
                     setattr(self, key, value)
+    def __update(self, id-None, sixe=None, x=None, y-None):
+        """method that uppdate atrribute"""
+        if id is not None:
+            self.id = id
+        if size is not None:
+            self.size = size
+        if x is not None:
+            self.x = x
+        if y is not None:
+            self.y = y
 
     def to_dictionary(self):
         """Returns the dictionary representation of a Square.
