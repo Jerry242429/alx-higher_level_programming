eb26daacc149bd0987b1ce5dafbf2548701e4528
Jerry242429 <eyerusalem008@gmail.com>
2024-02-13 11:23:30 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const languages = ["C is fun", "Python is cool", "JavaScript is amazing"]
-for (let i = 0; i < langauges.length; i++) {
-	console.log(languages[i]);
+const args = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const arg in args) {
+  console.log(args[arg]);
 }
